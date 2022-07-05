9e973f2216ce736a970456b17934872d61cc66d8
harperalmo <harperbenchcreek@gmail.com>
2022-07-04 23:47:47 -0600

    Initial code for marshaller_comm.py. It is not finished, but I like having a backup if needed...

--- marshaller_comm.py
+++ marshaller_comm.py
@@ -16,11 +16,13 @@ check for data in a queue that needs to be processed.
 from PyQt5 import QtCore
 from PyQt5.QtCore import pyqtSlot, QByteArray, QThread
 import serial
+import time
 import queue
 import post_office
 
 TO_MARSHALL_Q_SIZE    = 50
 TO_POST_OFFICE_Q_SIZE = 50
+SERIAL_TIMEOUT        = 0.1
 
 class MarshallerComm( QThread ):
     """MarshallerComm handles communications betweeen the marshaller component
@@ -43,17 +45,45 @@ class MarshallerComm( QThread ):
         """callback used by post office to start sending data across the
         communication channel to the marshaller component (esp32 via serial)."""
         self.to_marshall_q.put(letter)
-        print(f"Marshaller added letter to q. Letter is:")
+        print(f"Marshaller added letter to queue. Letter is:")
         print(f"To:      {letter.destination()}")
         print(f"From:    {letter.source()}")
         print(f"Content: {letter.content()}")
+    
+    def uart_receive(self, s):
+        msg = return s if type(s) is str else "".join([char(b) for b in s])
         
+    def str_bytes(s):
+        return s.encode('utf-8')
         
-#   def retrieve_marshaller_letter(self):
-#    ser = serial.Serial(port     = '/dev/serial0',
-#                            baudrate = 115200,
-#                            parity   = serial.PARITY_NONE,
-#                            stopbits = serial.STOPBITS_ONE,
-#                            bytesize = serial.EIGHTBITS,
-#                            timeout  = 0. 1 )
+    def uart_send(self, s ):
+        print(f"uart sending: {s}")
+        
+    def 
+        
+    def run(self):
+        """This is the async routine that is used for the thread process. It's
+        job is to manage the serial port and move messages to the appropriate
+        queues so other routines may process them."""
+        
+        self.uart = serial.Serial(port     = '/dev/serial0',
+                           baudrate = 115200,
+                           parity   = serial.PARITY_NONE,
+                           stopbits = serial.STOPBITS_ONE,
+                           bytesize = serial.EIGHTBITS,
+                           timeout  = SERIAL_TIMEOUT)
+        time.sleep(SERIAL_TIMEOUT*1.2)
+        self.uart.flushInput()
+        
+        while self.running:
+            s = self.uart.read(self.uart.in_waiting or 1)
+            if s:
+                self.uart_receive(bytes_str(s))
+            if not self.to_marshall_q.empty():
+                tx_data = str(self.to_marshall_q.get())
+                self.uart.write(self.str_bytes(txt))
+                
+        if self.uart:
+            self.uart.close()
+            self.uart = None
  
